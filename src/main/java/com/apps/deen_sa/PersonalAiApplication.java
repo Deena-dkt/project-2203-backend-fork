90a2223b4e1b967fe7c82e006c8d94f01f8df657
Deenadhayalanm22 <53555317+Deenadhayalanm22@users.noreply.github.com>
2025-12-05 12:17:30 +0530

    Update PersonalAiApplication.java

--- src/main/java/com/apps/deen_sa/PersonalAiApplication.java
+++ src/main/java/com/apps/deen_sa/PersonalAiApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PersonalAiApplication {
 
 	public static void main(String[] args) {
+		String port = System.getenv("PORT");
+		if (port == null) {
+			port = "8080"; // fallback for local dev
+		}
+		System.getProperties().put("server.port", port);
 		SpringApplication.run(PersonalAiApplication.class, args);
 	}
-
 }
