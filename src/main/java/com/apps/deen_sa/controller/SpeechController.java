7f088851146be11eb7d084000b46a9140f71cc53
Deenadhayalanm22 <53555317+Deenadhayalanm22@users.noreply.github.com>
2025-12-24 21:52:52 +0530

    Added some logs

--- src/main/java/com/apps/deen_sa/controller/SpeechController.java
+++ src/main/java/com/apps/deen_sa/controller/SpeechController.java
@@ -5,11 +5,13 @@ import com.apps.deen_sa.orchestrator.ConversationContext;
 import com.apps.deen_sa.orchestrator.SpeechOrchestrator;
 import com.apps.deen_sa.orchestrator.SpeechResult;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
+@Log4j2
 public class SpeechController {
 
     private final SpeechOrchestrator orchestrator;
@@ -18,6 +20,7 @@ public class SpeechController {
     @PostMapping("/process")
     public SpeechResult processSpeech(@RequestBody SpeechInput request) {
 
+        log.info("Message is about to be processed - {}", request.getText());
         return orchestrator.process(
                 request.getText(),
                 conversationContext
